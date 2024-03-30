fdfbf5099a1aad641ffd8b073677dd672de827d5
Алибек <gryteck@vk.com>
2024-03-30 23:39:20 +0300

    2.19 minor beta - solved redis connection

--- services/matchbot/database.py
+++ services/matchbot/database.py
@@ -6,10 +6,10 @@ from config import settings
 
 if settings.MODE == 'TEST':
     engine = create_async_engine(settings.TEST_PG_URL)
-    redis = aioredis.from_url(url=settings.TEST_RD_URL)
+    redis = aioredis.Redis(host=settings.TEST_RD_HOST, db=1, password=settings.TEST_RD_PASS)
 else:
     engine = create_async_engine(settings.PG_URL)
-    redis = aioredis.from_url(url=settings.RD_URL)
+    redis = aioredis.Redis(host=settings.RD_HOST, db=1, password=settings.RD_PASS)
 
 pg_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
