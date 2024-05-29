46cc927684c01db1906d2a6b34e118967784bc01
Алибек <gryteck@vk.com>
2024-05-29 15:47:08 +0300

    - fixed claims.py

--- services/matchbot/handlers/claims.py
+++ services/matchbot/handlers/claims.py
@@ -7,7 +7,7 @@ from db.crud import Postgre as db
 from db.redis_api import RedisDB as rd
 from db.states import States
 
-from .reactions import random_form
+from .reactions.random_form import random_form
 
 
 @dp.message_handler(state=States.claim)
@@ -18,9 +18,9 @@ async def claim(message: types.Message):
     f, l = await db.get_user(message.from_user.id), await db.get_user(
         (await rd.get_data(message.from_user.id)).liked_id)
     if message.text in ["1", "2"]:
-        if message.from_user.id not in l.noticed and l.id < 999:
-            f = await db.update_user(l.id, noticed=l.noticed + [message.from_user.id],
-                                     claims=l.claims + [int(message.text)])
+        if message.from_user.id not in l.noticed and l.id > 999:
+            f = await db.update_user(l.id, noticed=l.noticed+[message.from_user.id],
+                                     claims=l.claims+[message.text])
             await db.create_action(message.from_user.id, l.id, 'claim')
         if l.id in f.liked:
             f = await db.update_user(message.from_user.id, liked=f.liked[:-1])
@@ -44,12 +44,13 @@ async def claim_text(message: types.Message):
 
     l, f = await db.get_user(liked_id), await db.get_user(message.from_user.id)
 
-    if message.from_user.id not in l.noticed and l.id < 999:
-        await db.update_user(liked_id, noticed=l.noticed + [message.from_user.id], claims=l.claims + [3])
+    if message.from_user.id not in l.noticed and l.id > 999:
+        await db.update_user(liked_id, noticed=l.noticed+[message.from_user.id], claims=l.claims+['3'])
         await db.create_action(message.from_user.id, l.id, 'claim')
 
     await bot.send_photo(photo=l.photo, chat_id=settings.SUPPORT_ID,
-                         caption=f"#claim {liked_id}\n{t.cap(l)}\n\nFrom {message.from_user.id}:\n{message.text}")
+                         caption=t.adm_cap(l, "claim")+f"\n\nFrom {message.from_user.id}\nMessage:{message.text}",
+                         reply_markup=kb.admin(l))
     await message.answer(t.ban_thq)
-    # вывод анкеты
+
     await random_form(message, f)
